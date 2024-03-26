038afa6495db2f76481e18fdd8cb2fcae42e6df5
Carlos Cabas <ccabas@zilliant.com>
2024-03-26 08:51:40 -0500

    adding support to mqtt OTA

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from OTA_Wrapper import OTA_Wrapper
 from wifi_config import SSID, PASSWORD
 from time import sleep
+from umqttsimple import MQTTClient
 import network
+import machine
+
+BROKER="broker.hivemq.com"
+
 
 def connect_wifi():
     """ Connect to Wi-Fi."""
@@ -15,5 +20,23 @@ def connect_wifi():
     print(f'Connected to WiFi, IP is: {sta_if.ifconfig()[0]}')
 
 
+def process_message(topic, msg):
+    print(msg)
+
+
 connect_wifi()
-ota = OTA_Wrapper(github_url="https://github.com/ingcoty/tank_mmonitor.git")
+
+client_id = "ESP32_TankMonitor"
+mqtt_client = MQTTClient(client_id, BROKER)
+mqtt_client.set_callback(process_message)
+mqtt_client.connect()
+mqtt_client.subscribe("mqtt-github-action/tank_monitor")
+
+while True:
+    print("waiting for a github push...")
+    sleep(5)
+
+
+#ota = OTA_Wrapper(github_url="https://github.com/ingcoty/tank_mmonitor.git")
+
+
